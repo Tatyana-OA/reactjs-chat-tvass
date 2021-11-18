1f72426f4b7428fbe3362d048fc1b24917a7182d
Tatyana Asenova <tasenova@devrix.com>
2021-11-18 14:34:34 +0200

    Initial settings and connection - Chat SDK Project Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+	return (
+		// Setting props for the ChatEngine component  -> Project setup in https://chatengine.io/
+		<ChatEngine
+			height="100vh"
+			projectID="9987b00b-03ec-4586-b6a8-7f42bbfc5a82"
+			userName="Nexxita"
+			userSecret="bananas"
+		/>
+	)
+	}
 export default App;
